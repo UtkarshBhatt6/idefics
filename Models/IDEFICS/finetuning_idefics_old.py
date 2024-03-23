8a94f7592798bcde1efa41e18686760c859a0a96
UtkarshBhatt6 <utkarshbhatt@kgpian.iitkgp.ac.in>
2024-03-23 18:30:51 +0530

    *

--- Models/IDEFICS/finetuning_idefics_old.py
+++ Models/IDEFICS/finetuning_idefics_old.py
@@ -49,14 +49,17 @@ def ds_transforms(example_batch):
     for i in range(len(example_batch['query'])):
         # We split the captions to avoid having very long examples, which would require more GPU ram during training
         caption = example_batch['query'][i].split(".")[0]
-      
+        # img_name=example_batch['image'][i]
+        # # image_url="train/png/"+img_name
+        # image_url=path+img_name
+        # image = Image.open(image_url)
         curr_prompt= [
                 #
                 example_batch['image'][i],
                 # f"Question: {caption} Answer: Answer is {example_batch['label'][i]}.",
-                f"Question: {caption} Answer: {example_batch['label'][i]}",
+                f"Question: What's on the picture? Answer: This is {example_batch['label'][i]}. {caption}",
             ]
-        print(curr_prompt);
+        # print(f"currprompt is {i}: {curr_prompt}")
         prompts.append(
 
            curr_prompt
@@ -64,9 +67,102 @@ def ds_transforms(example_batch):
 
 
     inputs = processor(prompts, transform=image_transform, return_tensors="pt").to(device)
+    # print("printing input_ids: ",inputs["input_ids"])
     inputs["labels"] = inputs["input_ids"]
+    # print("inputs: ",inputs)
 
     return inputs
+# def ds_transforms(example_batch,path):
+#     image_size = processor.image_processor.image_size
+#     image_mean = processor.image_processor.image_mean
+#     image_std = processor.image_processor.image_std
+
+#     image_transform = transforms.Compose([
+#         convert_to_rgb,
+#         transforms.RandomResizedCrop((image_size, image_size), scale=(0.9, 1.0), interpolation=transforms.InterpolationMode.BICUBIC),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=image_mean, std=image_std),
+#     ])
+
+#     prompts = []
+#     for i in range(len(example_batch['sentence'])):
+#         # We split the captions to avoid having very long examples, which would require more GPU ram during training
+#         caption = example_batch['sentence'][i].split(".")[0]
+#         img_name=example_batch['image'][i]
+#         # image_url="train/png/"+img_name
+#         image_url=path+img_name
+#         image = Image.open(image_url)
+#         curr_prompt= [
+#                 #
+#                 image,
+#                 f"Question: {caption} Answer: Answer is {example_batch['text_label'][i]}.",
+#             ]
+#         print(f"currprompt is {i}: {curr_prompt}")
+#         prompts.append(
+
+#            curr_prompt
+#         )
+
+
+#     inputs = processor(prompts, transform=image_transform, return_tensors="pt").to(device)
+#     # print("printing input_ids: ",inputs["input_ids"])
+#     inputs["labels"] = inputs["input_ids"]
+#     print("inputs: ",inputs)
+
+#     return inputs
+
+class ChartQADataset(Dataset):
+    """ChartQA Dataset."""
+
+    def __init__(self, json_file, root_dir='./', transform=None):
+        """
+        Arguments:
+            json_file (string): Path to the csv file with annotations.
+
+        """
+        self.tacos_df = pd.read_json(json_file)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_name=self.tacos_df['imgname']
+        self.sentences = self.tacos_df['query']
+        #self.labels = self.tacos_df['Sentiment']
+        self.text_labels = self.tacos_df['label']
+        self.id = self.tacos_df.index
+        #self.abstracts = self.tacos_df['Aspect']
+
+    def __len__(self):
+        return len(self.tacos_df)
+
+    def __getitem__(self, idx):
+        # if torch.is_tensor(idx):
+        #     idx = idx.tolist()
+        # image=torch.tensor(self.image_name[idx])
+        # sentence=torch.tensor(self.sentences[idx]).squeeze()
+        # text_label=torch.tensor(self.text_labels[idx]).squeeze()
+        image=self.image_name.iloc[idx]
+        sentence = self.sentences.iloc[idx]
+        text_label = self.text_labels.iloc[idx]
+        id = self.tacos_df['id'].iloc[idx] 
+        #label = self.labels[idx]
+        #print('text label',text_label)
+        #abstract = self.abstracts[idx]
+
+        sample = {'id': id,'image':image, 'sentence': sentence,  'text_label': text_label}
+        return sample
+
+    def __repr__(self):
+        return f"ChartQADataset(num_samples={len(self)})"
+    
+# tacos_dataset_train = ChartQADataset(json_file='../../ChartQADataset/train/train_augmented.json')
+# tacos_dataset_val = ChartQADataset(json_file='../../ChartQADataset/val/val_augmented.json')
+# tacos_dataset_train = ChartQADataset(json_file='../../ChartQADataset/train/train_augmented_few.json')
+# tacos_dataset_val = ChartQADataset(json_file='../../ChartQADataset/val/val_augmented_few.json')
+
+# dataset_train = Dataset.from_dict(
+#         {"id":list(tacos_dataset_train.id),"image":list(tacos_dataset_train.image_name),"sentence": list(tacos_dataset_train.sentences), "text_label": list(tacos_dataset_train.text_labels)})
+# dataset_val = Dataset.from_dict(
+#         {"id":list(tacos_dataset_val.id),"image":list(tacos_dataset_val.image_name),"sentence": list(tacos_dataset_val.sentences), "text_label": list(tacos_dataset_val.text_labels)})
+
 
 
 # print('training completed')
@@ -85,6 +181,8 @@ bnb_config = BitsAndBytesConfig(
 )
 
 processor = AutoProcessor.from_pretrained(checkpoint, use_auth_token=False,cache_dir = '/NS/ssdecl/work/')
+# processor = AutoTokenizer.from_pretrained(checkpoint,use_fast=False, use_auth_token=False)
+# Simply take-off the quantization_config arg if you want to load the original model
 model = IdeficsForVisionText2Text.from_pretrained(checkpoint, quantization_config=bnb_config, device_map="auto",cache_dir = '/NS/ssdecl/work/')
 print(model)
 
@@ -96,6 +194,12 @@ config = LoraConfig(
     lora_dropout=0.05,
     bias="none",
 )
+# dataset_train=pd.read_json('../../ChartQADataset/train/train_augmented_few.json')
+# dataset_val=pd.read_json('../../ChartQADataset/val/val_augmented_few.json')
+# train_ds = ds_transforms(dataset_train,'../../ChartQADataset/train/png_few/')
+# eval_ds = ds_transforms(dataset_val,'../../ChartQADataset/val/png_few/')
+# train_ds =ds_transforms(dataset_train,'../../ChartQADataset/train/png/')
+# eval_ds =ds_transforms(dataset_val,'../../ChartQADataset/val/png/')
 model = get_peft_model(model, config)
 model.print_trainable_parameters()
 training_args = TrainingArguments(
