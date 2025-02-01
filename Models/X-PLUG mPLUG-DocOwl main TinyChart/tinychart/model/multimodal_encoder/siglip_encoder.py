07606d1b1b6c7923bef6f183e913066a7e736d0b
UtkarshBhatt6 <utkarshbhatt6@gmail.com>
2025-02-01 21:42:14 +0530

    rem changes

--- Models/X-PLUG mPLUG-DocOwl main TinyChart/tinychart/model/multimodal_encoder/siglip_encoder.py
+++ Models/X-PLUG mPLUG-DocOwl main TinyChart/tinychart/model/multimodal_encoder/siglip_encoder.py
@@ -659,8 +659,8 @@ class SigLipVisionModel(SigLipPreTrainedModel):
         >>> import requests
         >>> from transformers import AutoProcessor, SigLipVisionModel
 
-        >>> model = SigLipVisionModel.from_pretrained("google/siglip-base-patch16-224",cache_dir='/NS/ssdecl/work')
-        >>> processor = AutoProcessor.from_pretrained("google/siglip-base-patch16-224",cache_dir='/NS/ssdecl/work')
+        >>> model = SigLipVisionModel.from_pretrained("google/siglip-base-patch16-224")
+        >>> processor = AutoProcessor.from_pretrained("google/siglip-base-patch16-224")
 
         >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
